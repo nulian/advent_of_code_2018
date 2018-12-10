53dfa717b50a13af02447c762615b00b63870f4d
Peter Arentsen <peter.arentsen@outlook.com>
2018-12-10 22:13:53 +0100

    Add both solutions and all the data

--- lib/assign10_1.ex
+++ lib/assign10_1.ex
@@ -23,17 +23,29 @@ defmodule Assign10_1 do
 
 
   defmodule StarField do
-    defstruct stars: []
+    defstruct stars: [], previous_x_range: nil, seconds: 0
 
-    def move_starfield(%StarField{stars: stars}) do
+    def move_starfield(%StarField{stars: stars} = starfield) do
       stars = Enum.map(stars, fn star ->
         Star.apply_velocity(star)
       end)
 
-      %StarField{stars: stars}
+      %StarField{starfield | stars: stars, seconds: starfield.seconds + 1}
     end
 
-    def print_field(starfield) do
+    def loop_seconds(starfield) do
+      new_starfield = move_starfield(starfield)
+      {_, _, {maxx, minx, maxy, miny}} = get_max_size(new_starfield)
+      loop_starfield = if (maxx.position.x - minx.position.x) < starfield.previous_x_range do
+        loop_seconds(%StarField{new_starfield | previous_x_range: maxx.position.x - minx.position.x})
+      else
+        starfield
+      end
+
+      %StarField{loop_starfield | previous_x_range: maxx.position.x - minx.position.x}
+    end
+
+    def get_max_size(starfield) do
       {minx, maxx} = Enum.min_max_by(starfield.stars, fn star ->
         star.position.x
       end)
@@ -41,6 +53,11 @@ defmodule Assign10_1 do
         star.position.y
       end)
       {xrange, yrange} = {(minx.position.x)..(maxx.position.x), (miny.position.y)..(maxy.position.y)}
+      {xrange, yrange, {maxx, minx, maxy, miny}}
+    end
+
+    def print_field(starfield) do
+      {xrange, yrange, {maxx, _, _, _}} = get_max_size(starfield)
 
       lookup_map = starfield.stars |> Enum.group_by(& &1.position.y)
 
@@ -63,7 +80,7 @@ defmodule Assign10_1 do
     end
   end
 
-  def assignment do
+  def assignment1 do
     points = "data/assign10.data"
     |> File.read!()
     |> String.split("\r\n")
@@ -75,6 +92,22 @@ defmodule Assign10_1 do
     end)
 
     %StarField{stars: result}
+    |> StarField.loop_seconds() |> StarField.print_field
   end
 
+  def assignment2 do
+    points = "data/assign10.data"
+             |> File.read!()
+             |> String.split("\r\n")
+
+    result = Enum.map(points, fn item ->
+      [[p_x, p_y, v_x, v_y]] = Regex.scan(~r/position=<\s*(-?\d*),\s*(-?\d+)> velocity=<\s*(-?\d*),\s*(-?\d*)>/, item, capture: :all_but_first)
+
+      Star.new(Point.new(String.to_integer(p_x), String.to_integer(p_y)), Point.new(String.to_integer(v_x), String.to_integer(v_y)))
+    end)
+
+    %StarField{stars: result}
+    |> StarField.loop_seconds()
+    |> Map.get(:seconds)
+  end
 end
